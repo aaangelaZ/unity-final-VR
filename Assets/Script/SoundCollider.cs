418c6ef55bac37801de438db8bd3244a87ca3a41
Junru Angelina Xu <anhsuxjr@gmail.com>
2022-04-27 04:11:42 -0400

    adding Sound effects for Window && LockedDoor

---
+++ Assets/Script/SoundCollider.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundCollider : MonoBehaviour
+{
+    AudioSource _audioSource;
+    public AudioClip windSound;
+
+    void Start()
+    {
+        _audioSource = GetComponent<AudioSource>();
+    }
+
+ 
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Debug.Log("Player entered");
+            _audioSource.PlayOneShot(windSound);
+
+                }
+    }
+}
