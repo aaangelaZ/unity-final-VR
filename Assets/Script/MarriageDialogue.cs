81102a7daab50a17a949c92752d2b552f169e1f3
Junru Angelina Xu <anhsuxjr@gmail.com>
2022-05-03 22:03:27 -0400

    add marriage dialogue & sound

---
+++ Assets/Script/MarriageDialogue.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MarriageDialogue : MonoBehaviour
+{
+    public GameObject textBox;
+
+    AudioSource _audioSource;
+    public AudioClip dialogueSound;
+
+    void Start()
+    {
+        textBox.SetActive(false);
+        _audioSource = GetComponent<AudioSource>();
+    }
+
+    
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            textBox.SetActive(true);
+            _audioSource.PlayOneShot(dialogueSound);
+        }
+    }
+}
