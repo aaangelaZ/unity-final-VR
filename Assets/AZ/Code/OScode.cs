106cf5ff4bba64f0dcd0e180f8d31a1f343129c8
aaangelaZ <zhuc169@newschool.edu>
2022-05-10 04:20:30 -0400

    bedroom collider change

--- Assets/AZ/Code/OScode.cs
+++ Assets/AZ/Code/OScode.cs
@@ -6,12 +6,10 @@ using UnityEngine.UI;
 public class OScode : MonoBehaviour
 {
     public GameObject playerOsText;
-    public GameObject endingOsText;
     
     public void Start()
     {
         playerOsText.SetActive(false);
-        endingOsText.SetActive(false);
     }
 
     void OnTriggerEnter(Collider other)
@@ -19,13 +17,9 @@ public class OScode : MonoBehaviour
         if(other.tag == "Player")
         {
             playerOsText.SetActive(true);
-            endingOsText.SetActive(true);
 
             Destroy(playerOsText, 5f);
         }
-        else
-        {
-            endingOsText.SetActive(false);
-        }
+
     }
 }
