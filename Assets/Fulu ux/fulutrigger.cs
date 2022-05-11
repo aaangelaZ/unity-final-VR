c838223ae5f4ce00b966cc947876af0a1877d165
qdkf8126HA <102699139+wiuionly@users.noreply.github.com>
2022-05-11 08:16:55 -0400

    fulu debug

--- Assets/Fulu ux/fulutrigger.cs
+++ Assets/Fulu ux/fulutrigger.cs
@@ -10,7 +10,7 @@ public class fulutrigger : MonoBehaviour
     {
     // yield return new WaitForSeconds(0.5f);
 
-        for (int i=0; i<20; i++ )
+        for (int i=0; i<25; i++ )
         {
             Vector3 pos = new Vector3 (Random.Range(8.34f, 8.45f),Random.Range(1.32f,1.36f), Random.Range(4.11f, 5.11f));
                 
