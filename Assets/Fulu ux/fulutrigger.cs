6851dabc54e099b24d8ebbbbe03b038b5a544a74
qdkf8126HA <102699139+wiuionly@users.noreply.github.com>
2022-05-11 07:23:29 -0400

    fulu debug ipad screen update

--- Assets/Fulu ux/fulutrigger.cs
+++ Assets/Fulu ux/fulutrigger.cs
@@ -8,16 +8,16 @@ public class fulutrigger : MonoBehaviour
 
     IEnumerator papershow()
     {
-    yield return new WaitForSeconds(0.5f);
+    // yield return new WaitForSeconds(0.5f);
 
-    for (int i=0; i<20; i++ )
-    {
-        Vector3 pos = new Vector3 (Random.Range(8.34f, 8.45f),Random.Range(1.32f,1.36f), Random.Range(4.11f, 5.11f));
-            
-        // Instantiate(fuluPrefab, pos, Quaternion.identity);
-        Instantiate(fuluPrefab, pos, Random.rotation);
-        yield return new WaitForSeconds(0.1f);
-    }
+        for (int i=0; i<20; i++ )
+        {
+            Vector3 pos = new Vector3 (Random.Range(8.34f, 8.45f),Random.Range(1.32f,1.36f), Random.Range(4.11f, 5.11f));
+                
+            Instantiate(fuluPrefab, pos, Quaternion.identity);
+            // Instantiate(fuluPrefab, pos, Random.rotation);
+            yield return new WaitForSeconds(0.1f);
+        }
     }
 
     // public void SpawnSphere()
