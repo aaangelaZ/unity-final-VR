9579720716b0bc09cfa9b8168b07f03730fbda92
qdkf8126HA <102699139+wiuionly@users.noreply.github.com>
2022-05-11 08:22:48 -0400

    update

--- Assets/Fulu ux/fulutrigger.cs
+++ Assets/Fulu ux/fulutrigger.cs
@@ -14,8 +14,8 @@ public class fulutrigger : MonoBehaviour
         {
             Vector3 pos = new Vector3 (Random.Range(8.34f, 8.45f),Random.Range(1.32f,1.36f), Random.Range(4.11f, 5.11f));
                 
-            Instantiate(fuluPrefab, pos, Quaternion.identity);
-            // Instantiate(fuluPrefab, pos, Random.rotation);
+            // Instantiate(fuluPrefab, pos, Quaternion.identity);
+            Instantiate(fuluPrefab, pos, Random.rotation);
             yield return new WaitForSeconds(0.1f);
         }
     }
