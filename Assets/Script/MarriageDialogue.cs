ea9e2a4207a1aedf2f53f02f7880b8fa264a051d
Junru Angelina Xu <anhsuxjr@gmail.com>
2022-05-11 00:47:15 -0400

    update Dialogue system - debug Ending1

--- Assets/Script/MarriageDialogue.cs
+++ Assets/Script/MarriageDialogue.cs
@@ -5,31 +5,75 @@ using UnityEngine.UI;
 
 public class MarriageDialogue : MonoBehaviour
 {
-    public GameObject textBox;
+    public GameObject textBox1;
+    public GameObject textBoxA;
+    public GameObject textBoxB;
+
+    public bool dialogueOn;
 
     AudioSource _audioSource;
     public AudioClip dialogueSound;
 
     void Start()
     {
-        textBox.SetActive(false);
+        textBox1.SetActive(false);
         _audioSource = GetComponent<AudioSource>();
     }
 
-    
-    void Update()
-    {
-        
-    }
 
     void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            textBox.SetActive(true);
+            textBox1.SetActive(true);
+            dialogueOn = true; //open up dialogue system
             _audioSource.PlayOneShot(dialogueSound);
+        }
+    }
 
-            Destroy(textBox, 5f);
+
+
+    void Update()
+    {
+        if (dialogueOn)
+        {
+            if (OVRInput.Get(OVRInput.Button.One)) //press A button
+            {
+                Destroy(textBox1);
+                textBoxA.SetActive(true);
+                _audioSource.PlayOneShot(dialogueSound);
+
+                if (OVRInput.Get(OVRInput.Button.One)) //press A button
+                {
+                    Destroy(textBoxA);
+                    textBoxB.SetActive(true);
+                    _audioSource.PlayOneShot(dialogueSound);
+                }
+
+                else if (OVRInput.Get(OVRInput.Button.Two)) //press B button
+                {
+                    Destroy(textBoxA);
+                }
+            }
+
+            else if (OVRInput.Get(OVRInput.Button.Two)) //press B button
+            {
+                Destroy(textBox1);
+                textBoxB.SetActive(true);
+                _audioSource.PlayOneShot(dialogueSound);
+
+                if (OVRInput.Get(OVRInput.Button.One)) //press A button
+                {
+                    Destroy(textBoxB);
+                    textBoxA.SetActive(true);
+                    _audioSource.PlayOneShot(dialogueSound);
+
+                }
+                else if (OVRInput.Get(OVRInput.Button.Two)) //press B button
+                {
+                    Destroy(textBoxB);
+                }
+            }
         }
     }
 }
